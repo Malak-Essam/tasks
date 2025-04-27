9928bb5491671cdb58499acae0d5b0c25e9d174d
Malak-Essam <63660283+Malak-Essam@users.noreply.github.com>
2025-04-27 14:49:33 +0300

    Initial commit

---
+++ src/test/java/com/malak/tasks/TasksBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malak.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
