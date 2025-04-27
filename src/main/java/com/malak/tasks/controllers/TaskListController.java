a33145cf46a5d10a0ade69ee73b8647b487e3089
Malak-Essam <63660283+Malak-Essam@users.noreply.github.com>
2025-04-27 17:51:54 +0300

    add get tasklist by id action, and created a method in tasklistservice to return tasklist by id

--- src/main/java/com/malak/tasks/controllers/TaskListController.java
+++ src/main/java/com/malak/tasks/controllers/TaskListController.java
@@ -1,8 +1,11 @@
 package com.malak.tasks.controllers;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,6 +45,12 @@ public class TaskListController {
 	public TaskListDto createTaskList(@RequestBody TaskListDto taskListDto) {
 		TaskList createdTaskList = taskListService.createTaskList(taskListMapper.fromDto(taskListDto));
 		return taskListMapper.toDto(createdTaskList);
+	}
+	
+	@GetMapping("/{id}")
+	public Optional<TaskListDto> getTaskList(@PathVariable UUID id){
+		Optional<TaskList> taskListOptional = taskListService.getTaskList(id);
+		return taskListOptional.map(taskListMapper::toDto);
 	}
 	
 }
--- src/main/java/com/malak/tasks/services/TaskListService.java
+++ src/main/java/com/malak/tasks/services/TaskListService.java
@@ -1,10 +1,13 @@
 package com.malak.tasks.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import com.malak.tasks.domain.entities.TaskList;
 
 public interface TaskListService {
 	List<TaskList> listTaskLists();
 	TaskList createTaskList(TaskList taskList);
+	Optional<TaskList> getTaskList(UUID id);
 }
