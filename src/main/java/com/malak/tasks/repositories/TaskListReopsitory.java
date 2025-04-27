7e77d43514ef5fce7549ae3f5bc61bb50405df6c
Malak-Essam <63660283+Malak-Essam@users.noreply.github.com>
2025-04-27 15:47:11 +0300

    add repositories task and tasklist by extending JPA repository interface

---
+++ src/main/java/com/malak/tasks/repositories/TaskListReopsitory.java
@@ -0,0 +1,13 @@
+package com.malak.tasks.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.malak.tasks.domain.entities.TaskList;
+
+@Repository
+public interface TaskListReopsitory extends JpaRepository<TaskList, UUID>{
+
+}
