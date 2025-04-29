4f9b976257b01674d6aaaecfb9b1c87132c86d0b
Malak-Essam <63660283+Malak-Essam@users.noreply.github.com>
2025-04-29 03:13:44 +0300

    enhance api documentation adding summary and description for all the endpoints

--- src/main/java/com/malak/tasks/controllers/TaskListController.java
+++ src/main/java/com/malak/tasks/controllers/TaskListController.java
@@ -18,8 +18,12 @@ import com.malak.tasks.domain.entities.TaskList;
 import com.malak.tasks.mappers.TaskListMapper;
 import com.malak.tasks.services.TaskListService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping(path = "/api/task-lists")
+@Tag(name = "Task List Controller", description = "Manage tasklists: create, update, delete, and list tasklists")
 public class TaskListController {
 	private final TaskListService taskListService;
 	private final TaskListMapper taskListMapper;
@@ -34,6 +38,7 @@ public class TaskListController {
 	}
 	
 	@GetMapping
+	@Operation(summary = "Get a list of tasklists", description = "Gets a list of tasklists")
 	public List<TaskListDto> listTaskLists(){
 		 return taskListService
 				 .listTaskLists()
@@ -43,23 +48,27 @@ public class TaskListController {
 	}
 	
 	@PostMapping
+	@Operation(summary = "Create a new tasklist", description = "Adds a new tasklist")
 	public TaskListDto createTaskList(@RequestBody TaskListDto taskListDto) {
 		TaskList createdTaskList = taskListService.createTaskList(taskListMapper.fromDto(taskListDto));
 		return taskListMapper.toDto(createdTaskList);
 	}
 	
 	@GetMapping("/{task_list_id}")
+	@Operation(summary = "Gets a tasklist", description = "Gets  a tasklist")
 	public Optional<TaskListDto> getTaskList(@PathVariable("task_list_id") UUID taskListId){
 		Optional<TaskList> taskListOptional = taskListService.getTaskList(taskListId);
 		return taskListOptional.map(taskListMapper::toDto);
 	}
 		
 	@PutMapping("/{task_list_id}")
+	@Operation(summary = "Update tasklist details", description = "Updates tasklist details")
 	public TaskListDto updateTaskList(@PathVariable("task_list_id") UUID taskListId, @RequestBody TaskListDto newtaskListDto){
 		TaskList updatedTaskList = taskListService.updateTaskList(taskListId, taskListMapper.fromDto(newtaskListDto));
 		return taskListMapper.toDto(updatedTaskList);
 	}
 	@DeleteMapping("/{task_list_id}")
+	@Operation(summary = "Delete a tasklist", description = "Deletes a tasklist with all it's tasks")
 	public void deleteTaskList(@PathVariable("task_list_id") UUID taskListId) {
 		taskListService.deleteTaskList(taskListId);
 	}
--- src/main/java/com/malak/tasks/controllers/TasksController.java
+++ src/main/java/com/malak/tasks/controllers/TasksController.java
@@ -17,8 +17,12 @@ import com.malak.tasks.domain.entities.Task;
 import com.malak.tasks.mappers.TaskMapper;
 import com.malak.tasks.services.TaskService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/api/task-lists/{task_list_id}/tasks")
+@Tag(name = "Task Controller", description = "Manage tasks: create, update, delete, and list tasks")
 public class TasksController {
 	
 	private final TaskService taskService;
@@ -30,22 +34,27 @@ public class TasksController {
 	}
 	
 	@GetMapping
+	@Operation(summary = "Get all tasks", description = "Returns a list of all tasks")
 	public List<TaskDto> listTasks(@PathVariable("task_list_id") UUID taskListId){
 		return taskService.listTasks(taskListId).stream().map(taskMapper::toDto).toList();
 	}
 	
 	@PostMapping
+	@Operation(summary = "Create a new task", description = "Adds a new task to the tasklist")
 	public TaskDto createTask(@PathVariable("task_list_id") UUID taskListId,
 			@RequestBody TaskDto taskDto) {
 		Task task = taskService.createTask(taskListId, taskMapper.fromDto(taskDto));
 		return taskMapper.toDto(task);
 	}
+	
 	@GetMapping("/{task_id}")
+	@Operation(summary = "get a task", description = "Gets a task from a list of tasks in a tasklist")
 	public TaskDto getTask(@PathVariable("task_list_id") UUID taskListId,
 			@PathVariable("task_id") UUID taskId) {
 		return taskService.getTask(taskListId, taskId).map(taskMapper::toDto).orElse(null);
 	}
 	@PutMapping("/{task_id}")
+	@Operation(summary = "Update a task", description = "Updates a task details")
 	public TaskDto updateTask(
 			@PathVariable("task_list_id") UUID taskListId,
 			@PathVariable("task_id") UUID taskId,
@@ -55,6 +64,7 @@ public class TasksController {
 	}
 	
 	@DeleteMapping("/{task_id}")
+	@Operation(summary = "Delete a task", description = "Delete a task from tasklist")
 	public void deleteTask(
 			@PathVariable("task_list_id") UUID taskListId,
 			@PathVariable("task_id") UUID taskId
